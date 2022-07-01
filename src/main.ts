97a69cc766caa65571a582850cb12ba3903d9d1c
mohneesh-nishad <mohneesh@noborderz.com>
2022-07-01 19:08:13 +0530

    [ADD] swagger module added

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,43 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { contentParser } from 'fastify-multer';
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
+
+
+const swaggerDocument = new DocumentBuilder()
+  .setTitle('API')
+  .setDescription('API')
+  .setVersion('1.0')
+  .addTag('API')
+  .build();
+
+const swagger_options: SwaggerDocumentOptions = {
+  operationIdFactory: (
+    controllerKey: string,
+    methodKey: string
+  ) => methodKey,
+};
+const customOptions: SwaggerCustomOptions = {
+  swaggerOptions: {
+    persistAuthorization: true,
+  },
+  customSiteTitle: 'My API Docs',
+};
 
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   app.register(contentParser)
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}/graphql`);
+
+  SwaggerModule.setup(
+    'api',
+    app,
+    SwaggerModule.createDocument(app, swaggerDocument, swagger_options, customOptions),
+  );
+
+  app.listen(3000).then(async (server) => {
+    console.log(`Application is running on: ${await app.getUrl()}/graphql`);
+  });
 }
 bootstrap();
