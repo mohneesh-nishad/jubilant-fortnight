881b9cfa169df86b62313d02126cc1a37141e09e
mohneesh-nishad <mohneesh@noborderz.com>
2022-07-01 17:22:35 +0530

    [ADD] content parser for multipart

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { contentParser } from 'fastify-multer';
 
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  app.register(contentParser)
   app.useGlobalPipes(new ValidationPipe());
-
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}/graphql`);
 }
